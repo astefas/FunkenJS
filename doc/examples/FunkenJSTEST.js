403f990a8b3b4ea8dcd060be2e37c0ca82fb1f65
astefas <alexander.stefas@stefas.de>
2019-03-12 18:19:39 +0100

    so now....
    it is a library

--- doc/examples/FunkenJSTEST.js
+++ doc/examples/FunkenJSTEST.js
@@ -10,14 +10,18 @@ fnkDev01.connectToPort("/dev/tty.usbmodem143301", 57600);
 fnkDev01.on("de.stefas.funken.serial.data", function(msg){
   console.log(msg);
 });
+
 fnkDev01.on("de.stefas.funken.serial.connectionError", function(msg){
   console.log("There has been an error establishing a connection on the COMPORT");
   console.log(msg);
 });
+
 fnkDev01.on("de.stefas.funken.serial.comportOpened", function(msg){
   console.log("COMPORT has been opened");
   console.log(msg);
 });
+
 fnkDev01.on("de.stefas.funken.serial.comportClosed", function(msg){
   console.log("COMPORT has been closed");
   console.log(msg);
+});
