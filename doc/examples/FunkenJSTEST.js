2c1fc04155b0db997aa4e5e5400deac1c11bce30
astefas <alexander.stefas@stefas.de>
2019-03-12 17:33:29 +0100

    because of comming classes i finally decided for...
    de.stefas.funken as classpath which is used as messagetypes for events and emits...

--- doc/examples/FunkenJSTEST.js
+++ doc/examples/FunkenJSTEST.js
@@ -7,18 +7,18 @@ FunkenJS.listPorts();
 //WELL... THIS AHS TO BE ALTERED OBVIOUSLY
 fnkDev01.connectToPort("/dev/tty.usbmodem143301", 57600);
 
-fnkDev01.on("de.funken.serial.data", function(msg){
+fnkDev01.on("de.stefas.funken.serial.data", function(msg){
   console.log(msg);
 });
-fnkDev01.on("de.funken.serial.connectionError", function(msg){
+fnkDev01.on("de.stefas.funken.serial.connectionError", function(msg){
   console.log("There has been an error establishing a connection on the COMPORT");
   console.log(msg);
 });
-fnkDev01.on("de.funken.serial.comportOpened", function(msg){
+fnkDev01.on("de.stefas.funken.serial.comportOpened", function(msg){
   console.log("COMPORT has been opened");
   console.log(msg);
 });
-fnkDev01.on("de.funken.serial.comportClosed", function(msg){
+fnkDev01.on("de.stefas.funken.serial.comportClosed", function(msg){
   console.log("COMPORT has been closed");
   console.log(msg);
 });
